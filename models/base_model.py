468962f18f9a94a6f87c7ce9f3491bf0cafe1984
timishot <timidotcom58@gmail.com>
2023-07-14 22:46:53 +0100

    more on base

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,18 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        iso_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        if (kwargs) != 0:
+            for k , v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    self.__dict__[k] = datetime.strptime(v, iso_format)
+                else:
+                    self.__dict__[k] = v
 
 
     def save(self):
