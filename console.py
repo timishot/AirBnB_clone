8a9d3e85091f223a05dbca79241ba8fb98199066
timishot <timidotcom58@gmail.com>
2023-07-15 15:43:28 +0100

    more on console

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """quit console"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
