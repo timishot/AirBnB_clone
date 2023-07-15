20494ba9196dcc1fd410eca0642c67156f46256c
timishot <timidotcom58@gmail.com>
2023-07-15 14:04:33 +0100

    more on base

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+from models import storage
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.strptime(v, iso_format))
+                else:
+                    setattr(self, k, v)
+        else:
+            storage.new(self)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
