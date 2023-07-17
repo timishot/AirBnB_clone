4fd3f2926b71720f06cbc3afa5ba19f73b0fade6
timishot <timidotcom58@gmail.com>
2023-07-17 03:39:04 +0100

    update the engine file and console file with module

--- console.py
+++ console.py
@@ -5,13 +5,25 @@ import sys
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import  State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     model_classes = {
             "BaseModel": BaseModel,
-            "User" : User
+            "User" : User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
             }
 
     def do_create(self, line):
